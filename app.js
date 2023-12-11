d699a712574176e68222153af47a1006874dbe16
KingHenrycruz <KingHenrycruz@gmail.com>
2023-12-11 16:59:49 +0100

    Code's cleaned up and well commented

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
+// Load environment variables from the .env file
 require('dotenv').config();
+
+// Import required modules
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 
 
+// Create an instance of the Express application
 const app = express();
 
+// Use bodyParser middleware to parse request bodies
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Define a route for the home page
 app.get("/", function(req, res){
+    // Send the index.html file when accessing the root route
    res.sendFile(__dirname + "/index.html")
 });
 
-
+// Define a route to handle POST requests
 app.post("/", async function(req, res){
+    // Extract the city name from the request body
     const query = req.body.cityName
+
+    // Retrieve the OpenWeatherMap API key from the environment variables
     const apiKey = process.env.API_KEY;
     const unit = "metric"
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
@@ -24,6 +34,8 @@ app.post("/", async function(req, res){
 
         response.on("data", function(data){
             const weatherData = JSON.parse(data)
+
+            // Extract relevant weather information
             const temp = weatherData.main.temp
             const weatherDescription = weatherData.weather[0].description
             const icon = weatherData.weather[0].icon
@@ -31,6 +43,9 @@ app.post("/", async function(req, res){
             // res.write("<p>The weather is currently " + weatherDescription + "<p>");
             // res.write("<h1>The temperature in " + query + " is " + temp + "degrees Celcius.</h1>");
             // res.write("<img src=" + imageURL + ">");
+
+
+            // Construct the HTML response to be sent
             const responseHTML = `
                 <div style="text-align: center; margin-top: 20px; background-color: #616263; color: #DFDBDB; padding: 20px;">
                     <h1 style="font-family: 'Lobster', sans-serif; font-size: 3.5rem; margin-bottom: -1rem;">WeatherWise</h1>
@@ -46,6 +61,8 @@ app.post("/", async function(req, res){
                     </a>
                 </div>
             `;
+
+            // Send the constructed HTML response
             res.send(responseHTML)
         })
     })
@@ -53,6 +70,7 @@ app.post("/", async function(req, res){
 })
 
 
+// Start the Express server and listen on port 3000
 app.listen(3000, function() {
     console.log("Listening on port 3000");
 })
